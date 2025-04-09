cbadea0cb0a388b96392bc497c6e18bc8dbbf976
MaxEngine <99622507+MaxEngineBP@users.noreply.github.com>
2025-04-09 10:44:54 +0300

    Create SuperSkyMobsCommand.java

---
+++ com.maxengine.superskymobs/commands/SuperSkyMobsCommand.java
@@ -0,0 +1,26 @@
+package com.maxengine.superskymobs.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import ru.xezard.vanillahard.VanillaHardPlugin;
+
+public class SuperSkyMobsCommand implements CommandExecutor {
+  private SuperSkyMobsPlugin plugin;
+  
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] arguments) {
+    if (arguments.length != 1)
+      return false; 
+    if (!sender.hasPermission("superskymobs.commands.reload")) {
+      sender.sendMessage("You don't have enough permissions!");
+      return false;
+    } 
+    this.plugin.reload();
+    sender.sendMessage("Plugin successfully reloaded!");
+    return true;
+  }
+  
+  public SuperSkyMobsCommand(SuperSkyMobsPlugin plugin) {
+    this.plugin = plugin;
+  }
+}
