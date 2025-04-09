b86a3dcffc10b9bfad8d0a221fa5d0c183c2b4c1
MaxEngine <99622507+MaxEngineBP@users.noreply.github.com>
2025-04-09 11:03:15 +0300

    Update SuperSkyMobsPlugin.java

--- com.maxengine.superskymobs/SuperSkyMobsPlugin.java
+++ com.maxengine.superskymobs/SuperSkyMobsPlugin.java
@@ -11,23 +11,48 @@ import com.maxengine.superskymobs.listeners.CreatureSpawnListener;
 import com.maxengine.superskymobs.listeners.EntityDeathListener;
 
 public class SuperSkyMobsPlugin extends JavaPlugin {
+  private CreatureSpawnListener creatureSpawnListener;
+  private EntityDeathListener entityDeathListener;
+
+  @Override
   public void onEnable() {
     saveDefaultConfig();
     registerListeners();
     registerCommands();
+    getLogger().info("SuperSkyMobs has been enabled!");
+  }
+  
+  @Override
+  public void onDisable() {
+    getLogger().info("SuperSkyMobs has been disabled!");
   }
   
   private void registerListeners() {
     PluginManager pluginManager = Bukkit.getPluginManager();
-    pluginManager.registerEvents((Listener)new CreatureSpawnListener(getConfig()), (Plugin)this);
-    pluginManager.registerEvents((Listener)new EntityDeathListener(getConfig()), (Plugin)this);
+    
+    creatureSpawnListener = new CreatureSpawnListener(getConfig());
+    entityDeathListener = new EntityDeathListener(getConfig());
+    
+    pluginManager.registerEvents(creatureSpawnListener, this);
+    pluginManager.registerEvents(entityDeathListener, this);
   }
   
   private void registerCommands() {
-    getCommand("superskymobs").setExecutor((CommandExecutor)new SuperSkyMobsCommand(this));
+    getCommand("superskymobs").setExecutor(new SuperSkyMobsCommand(this));
   }
   
   public void reload() {
     reloadConfig();
+    
+    // Обновляем конфигурацию для слушателей
+    if (creatureSpawnListener != null) {
+      creatureSpawnListener.reloadConfig(getConfig());
+    }
+    
+    if (entityDeathListener != null) {
+      entityDeathListener.reloadConfig(getConfig());
+    }
+    
+    getLogger().info("SuperSkyMobs configuration reloaded!");
   }
 }
