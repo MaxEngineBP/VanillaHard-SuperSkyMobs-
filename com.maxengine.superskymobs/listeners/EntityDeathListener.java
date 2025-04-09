dd16caa8ec5ec5de7bb4f715cdc53ec851797430
MaxEngine <99622507+MaxEngineBP@users.noreply.github.com>
2025-04-09 11:11:18 +0300

    Update EntityDeathListener.java

--- com.maxengine.superskymobs/listeners/EntityDeathListener.java
+++ com.maxengine.superskymobs/listeners/EntityDeathListener.java
@@ -4,10 +4,13 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import com.maxengine.superskymobs.utils.Random;
 
 public class EntityDeathListener implements Listener {
@@ -15,8 +18,19 @@ public class EntityDeathListener implements Listener {
   
   @EventHandler
   public void onDeath(EntityDeathEvent event) {
-    EntityType entityType = event.getEntityType();
-    Location location = event.getEntity().getLocation();
+    LivingEntity entity = event.getEntity();
+    EntityType entityType = entity.getType();
+    Location location = entity.getLocation();
+    
+    // Process head drops
+    handleHeadDrops(entityType, location);
+    
+    // We don't need to handle equipment drops here because Minecraft's native
+    // drop system will respect the drop chances we've set during mob spawn
+    // in CreatureSpawnListener
+  }
+  
+  private void handleHeadDrops(EntityType entityType, Location location) {
     Material material = null;
     switch (entityType) {
       case SKELETON:
@@ -32,27 +46,38 @@ public class EntityDeathListener implements Listener {
         material = Material.CREEPER_HEAD;
         break;
       case PIGLIN:
-        // Проверяем доступность Material.PIGLIN_HEAD для версии 1.21
+        // Check if PIGLIN_HEAD is available (Minecraft 1.21+)
         try {
           material = Material.valueOf("PIGLIN_HEAD");
         } catch (IllegalArgumentException e) {
-          // Если нет такого значения в Enum, значит версия не поддерживает эту голову
+          // If no such value in Enum, this version doesn't support this head
           material = null;
         }
         break;
     } 
+    
     if (material == null)
-      return; 
-    dropHead(location, material);
-  }
-  
-  private void dropHead(Location location, Material material) {
-    if (!Random.checkChance(this.configuration.getInt("Chances.Head-drop"), 100))
-      return; 
+      return;
+      
+    if (!configuration.getBoolean("Heads.Enabled", true))
+      return;
+      
+    // Get head drop chance from config, defaulting to the global head-drop chance
+    int dropChance = configuration.getInt("Heads.Types." + entityType.name() + ".Chance", 
+                                          configuration.getInt("Chances.Head-drop", 35));
+                                          
+    if (!Random.checkChance(dropChance, 100))
+      return;
+      
     location.getWorld().dropItemNaturally(location, new ItemStack(material));
   }
   
   public EntityDeathListener(FileConfiguration configuration) {
+    this.configuration = configuration;
+  }
+  
+  // Method to reload configuration
+  public void reloadConfig(FileConfiguration configuration) {
     this.configuration = configuration;
   }
 }
